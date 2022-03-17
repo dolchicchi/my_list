18f51299ea55de5cdceb903e1233f48f085b3a6a
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-17 17:54:38 +0900

    ingredientsテーブル作成

---
+++ db/migrate/20220317085010_create_ingredients.rb
@@ -0,0 +1,11 @@
+class CreateIngredients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ingredients do |t|
+      t.string     :string, null: false
+      t.integer    :amount
+      t.integer    :unit_id
+      t.references :recipe,  null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_080822) do
+ActiveRecord::Schema.define(version: 2022_03_17_085010) do
+
+  create_table "ingredients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "string", null: false
+    t.integer "amount"
+    t.integer "unit_id"
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
 
   create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -34,5 +44,6 @@ ActiveRecord::Schema.define(version: 2022_03_17_080822) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ingredients", "recipes"
   add_foreign_key "recipes", "users"
 end
