0bcc32dde1d79f178fc30239c417efe569e2a42e
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-17 18:15:51 +0900

    レシピ登録機能の実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,20 @@
 class RecipesController < ApplicationController
-  def new
 
+  def new
+    @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :source).merge(user_id: current_user.id)
+  end
 end
