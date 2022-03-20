a3d02453bf7a83624c918f06b2eb0a4bd37330ad
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-21 08:49:54 +0900

    レシピ編集機能の実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,11 +5,11 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe_ingredient = RecipeIngredient.new(recipe_params)
+    @recipe_ingredient = RecipeIngredient.new(recipe_ingredient_params)
     
     if @recipe_ingredient.valid?
       @recipe_ingredient.save
-      redirect_to root_path
+      redirect_to recipes_path
     else
       render :new
     end
@@ -23,13 +23,25 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
-  
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipes_path
+    else
+      render :edit
+    end
+  end
 
   private
-  def recipe_params
+  def recipe_ingredient_params
     params.require(:recipe_ingredient).permit(
       :title, :source,
       name: [], amount: [], unit_id: []
     ).merge(user_id: current_user.id)
   end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :source).merge(user_id: current_user.id)
+  end
+
 end
