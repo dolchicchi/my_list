2558717047cba5644368412e0a1a00293362de5c
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-22 12:21:56 +0900

    createアクション設定

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,5 +6,22 @@ class ListsController < ApplicationController
     @today = Date.today
   end
 
+  def select
+    @list = List.new
+    @recipe_lists = Recipe.all
+    @date = params[:date]
+  end
+
+  def create
+    list = List.new(list_params)
+    if list.save
+      redirect_to new_list_path
+    end
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:recipe_id).merge(user_id: current_user.id, date: params[:date])
+  end
 
 end
