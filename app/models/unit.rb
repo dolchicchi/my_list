ac101601a08937daa360ecb30fc4ee613dc3d834
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-24 11:27:35 +0900

    分量の単位追加

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,7 +3,10 @@ class Unit < ActiveHash::Base
     {id: 1, name: '---'},
     {id: 2, name: 'グラム'},
     {id: 3, name: 'コ'},
-    {id: 4, name: '本'}
+    {id: 4, name: '本'},
+    {id: 5, name: '束'},
+    {id: 6, name: 'cc'},
+    {id: 7, name: 'カップ'}
   ]
 
   include ActiveHash::Associations
