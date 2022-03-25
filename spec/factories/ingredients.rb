114050ff108a60d08e18d6ed1d76529e51c3b73d
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-25 19:18:29 +0900

    ingredient　ファクトリーボット設定

--- spec/factories/ingredients.rb
+++ spec/factories/ingredients.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :ingredient do
-    
+    name           { Faker::Name.name }
+    amount         { Faker::Number.between(from: 1, to: 999) }
+    unit_id        { Faker::Number.between(from: 2, to: 7) }
   end
 end
