89c3a1ba76ff277c1a342e4f8c2693868f244ca5
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-29 10:41:20 +0900

    モデル　コントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_081504) do
+ActiveRecord::Schema.define(version: 2022_03_29_013831) do
+
+  create_table "folders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_folders_on_user_id"
+  end
 
   create_table "ingredients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +62,7 @@ ActiveRecord::Schema.define(version: 2022_03_23_081504) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "folders", "users"
   add_foreign_key "ingredients", "recipes"
   add_foreign_key "lists", "recipes"
   add_foreign_key "lists", "users"
