9c3d8a761e948ee47dd9cd04327878d4ac04ecb2
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-29 18:05:42 +0900

    アソシエーション設定

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,3 +1,4 @@
 class Folder < ApplicationRecord
-  has_many :recipes
+  has_many :recipes, dependent: :nullify
+  belongs_to :user
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  belongs_to :folder
+  belongs_to :folder, optional: true
   has_many :ingredients, dependent: :destroy
   has_many :lists, dependent: :destroy
 end
