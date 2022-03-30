771150615d51ffb8371fedf8cda6d2b13a544f32
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-30 17:03:20 +0900

    アクティブハッシュアソシエーション

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,9 @@ class Recipe < ApplicationRecord
   belongs_to :folder, optional: true
   has_many :ingredients, dependent: :destroy
   has_many :lists, dependent: :destroy
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :genre
+  belongs_to :type
 end
