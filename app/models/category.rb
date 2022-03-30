395f7ae76dbaf3a9110e6d3b2efaadcd33737d6e
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-30 17:03:05 +0900

    アクティブハッシュ追加

---
+++ app/models/category.rb
@@ -0,0 +1,11 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '主菜' },
+    { id: 2, name: '副菜' },
+    { id: 3, name: 'スープ' },
+    { id: 4, name: 'その他' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :recipes
+end
