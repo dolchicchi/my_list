9beeb08a3e94505e26eff5d9ae1e57b0b8bd3062
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-04-01 18:19:35 +0900

    フォルダコントローラー　リファクタリング

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,8 +1,7 @@
 class FoldersController < ApplicationController
-  before_action :select_recipes, only: :add_recipe_select
-  before_action :set_folder, only: [:show, :edit, :destroy, :update, :add_recipe_select]
-  before_action :params_check, only: :add_recipe
-  
+  before_action :set_folder, except: [:new, :create, :index]
+  before_action :no_select_check, only: :add_recipe
+
   def new
     @folder = Folder.new
   end
@@ -22,8 +21,7 @@ class FoldersController < ApplicationController
 
   def show
     @recipes = @folder.recipes.order(updated_at: :desc)
-    @q = @recipes.ransack(params[:q])
-    @search_folder_recipes = @q.result(distinct: true)
+    @folder_included_recipes = search_hit_recipe(@recipes)
   end
 
   def edit
@@ -43,50 +41,23 @@ class FoldersController < ApplicationController
   end
 
   def add_recipe_select
-
+    recipes = Recipe.not_included_folder(current_user.id, @folder.id)
+    @choice_recipes = search_hit_recipe(recipes)
   end
-
-  def add_recipe
-    add_recipe_date
-    @recipe_ids.each do |recipe_id|
-      recipe = Recipe.find(recipe_id)
-      unless recipe.update(@folder_id)
-        render :add_recipe_select
-        return
-      end
-    end
-    redirect_to folder_path(params[:id])
-  end
-
-
   
   private
   def set_folder
     @folder = Folder.find(params[:id])
   end
 
-  def params_check
-    if params[:folder].blank?
-      redirect_to add_recipe_select_folder_path(params[:id])
-    end
-  end
-
-  def add_recipe_date
-    @recipe_ids = params.require(:folder).permit(recipe_ids: [])[:recipe_ids]
-    @folder_id = {folder_id: params[:id]}
-  end
-
   def folder_params
     params.require(:folder).permit(:title).merge(user_id: current_user.id)
   end
 
-  def select_recipes
-    @recipes = 
-    Recipe.where(user_id: current_user.id).where.not(folder_id: params[:id])
-    .or(
-      Recipe.where(user_id: current_user.id).where(folder_id: nil)
-    )
-    @q = @recipes.ransack(params[:q])
-    @select_recipes = @q.result(distinct: true)
+  # 検索結果を返す(引数に絞り込みの対象を渡す)Gem:Ransack使用
+  def search_hit_recipe(recipes)
+    @q = recipes.ransack(params[:q])
+    @q.result(distinct: true)
   end
+  
 end
