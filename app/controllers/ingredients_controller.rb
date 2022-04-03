39a06d6fe9639ce72809b46ee59d3fe6884efeb8
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-04-03 15:18:45 +0900

    Ingredients関連　リファクタリング

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,18 +1,15 @@
 class IngredientsController < ApplicationController
   before_action :new_ingredient, only: [:new, :create]
+  before_action :recipe_set, only: [:new, :create, :edit, :update]
+  before_action :user_check, only: :edit
   before_action :ingredient_set, only: [:edit, :update, :destroy]
-  before_action :recipe_set, only: [:new, :create,:edit, :update]
-  before_action :user_match?, only: [:edit, :update, :destroy]
   
   def new
   end
 
   def create
-    ingredients.each do |ingredient|
-      header = ["name", "amount", "unit_id"]
-      ingredient_parameter = Hash[header.zip(ingredient)]
-      ingredient_parameter["recipe_id"] = params[:recipe_id]
-      ingredient = Ingredient.new(ingredient_parameter)
+    ingredient_create_dates_ary.each do |ingredient_date|
+      ingredient = Ingredient.new(ingredient_date)
       unless ingredient.save
         render :new
         return
@@ -26,7 +23,7 @@ class IngredientsController < ApplicationController
   end
 
   def update
-    if @ingredient.update(ingredient_params)
+    if @ingredient.update(ingredient_edit_dates)
       redirect_to recipes_path
     else
       render :edit
@@ -40,7 +37,13 @@ class IngredientsController < ApplicationController
 
   private
   def new_ingredient
-   @ingredient = Ingredient.new
+    @ingredient = Ingredient.new
+  end
+
+  def user_check
+    unless current_user.id == @recipe.user_id
+      redirect_to root_path
+    end
   end
 
   def ingredient_set
@@ -51,28 +54,36 @@ class IngredientsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
   end
 
-  def ingredient_params
-    ingredients = params.require(:ingredient).permit(:name, :amount, :unit_id).merge(recipe_id: params[:recipe_id])
-    ingredients[:amount].tr!("０-９", "0-9")
-    ingredients
+  # 一回のリクエストで複数のレコードに保存するための処理 2
+  # データ保存できる形のハッシュを作り配列に格納
+  def ingredient_create_dates_ary
+    ingredients_params = params_shape_change
+    ary = []
+    ingredients_params.each do |ingredient_params|
+      header = ["name", "amount", "unit_id"]
+      ingredient_parameter = Hash[header.zip(ingredient_params)]
+      ingredient_parameter["recipe_id"] = params[:recipe_id]
+      ary << ingredient_parameter
+    end
+    return ary
   end
 
-  def ingredients
-    ingredients_params = params[:recipe_ingredient]
-    names = ingredients_params[:name]
-    amounts = ingredients_params[:amount]
-    amounts.each do |amount|
+  # 一回のリクエストで複数のレコードに保存するための処理 1
+  # 送られてきた配列のパラメーターをname amount unit_id 一つずつ取り出して配列に格納
+  def params_shape_change
+    params[:recipe_ingredient][:amount].each do |amount|
       amount.tr!("０-９", "0-9")
     end
-    unit_ids = ingredients_params[:unit_id]
-    ingredients = [names, amounts, unit_ids].transpose
+
+    return[
+      params[:recipe_ingredient][:name], 
+      params[:recipe_ingredient][:amount],
+      params[:recipe_ingredient][:unit_id]
+    ].transpose
   end
 
-  def user_match?
-    recipe = Recipe.find(params[:recipe_id])
-    unless current_user.id == recipe.user_id
-      redirect_to root_path
-    end
+  def ingredient_edit_dates
+    params[:ingredient][:amount].tr!("０-９", "0-9")
+    params.require(:ingredient).permit(:name, :amount, :unit_id).merge(recipe_id: params[:recipe_id])
   end
-
 end
