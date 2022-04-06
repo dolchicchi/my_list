01be7a5dc1291c76b3aea09df3dd8fa7004c38d9
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-04-06 19:04:53 +0900

    Listsコントローラーテストコード実装

---
+++ spec/requests/lists_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe "Lists", type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+    @recipe = FactoryBot.create(:recipe, user_id: @user.id)
+    @list = FactoryBot.create(:list, user_id: @user.id, recipe_id: @recipe.id)
+    sleep 0.1
+  end
+
+  describe 'GET #index' do
+    context "ログインしている場合" do
+      before do
+        sign_in @user
+      end
+      it 'リクエストすると正常にレスポンスが返ってくる' do 
+        get lists_path
+        expect(response.status).to eq 200
+      end
+      it 'レスポンスに登録済みのフォルダのタイトルが存在する' do 
+        get lists_path
+        expect(response.body).to include(@recipe.title)
+      end
+    end
+    context "ログインしていない場合" do
+      it '別のアクションへ遷移される' do 
+        get lists_path
+        expect(response.status).to eq 302
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    context "ログインしている場合" do
+      before do
+        sign_in @user
+      end
+      it 'リクエストすると正常にレスポンスが返ってくる' do 
+        get new_list_path
+        expect(response.status).to eq 200
+      end
+      it 'レスポンスに登録済みのフォルダのタイトルが存在する' do 
+        get new_list_path
+        expect(response.body).to include(@recipe.title)
+      end
+    end
+    context "ログインしていない場合" do
+      it '別のアクションへ遷移される' do 
+        get lists_path
+        expect(response.status).to eq 302
+      end
+    end
+  end
+end
