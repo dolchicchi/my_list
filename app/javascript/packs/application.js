11a3697bf4b9a15c1115f533a80fbe172fe32fe3
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-04-26 21:35:51 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,8 +13,7 @@ require("../guest_user_login")
 require("../text_change")
 require("../pull_down")
 require("../color_change")
-import "bootstrap"
-import "../stylesheets/application"
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
