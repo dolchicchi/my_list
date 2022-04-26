915a10322323bec6b124181f98259af413919837
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-04-26 10:38:22 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,3 +60,9 @@
 #     # password: "please use keys"
 #   }
 server '13.231.2.50', user: 'ec2-user', roles: %w{app db web}
+
+set :ssh_options, {
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
