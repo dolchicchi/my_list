cac220fbb8b3b0be798d120fb1c528b99c4d6bde
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-04-27 10:16:12 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,9 +4,7 @@ set :application, 'my_list'
 
 set :repo_url, 'git@github.com:dolchicchi/my_list.git'
 
-set :linked_dirs,
-    fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system',
-                                 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
